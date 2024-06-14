5faf56251fca3adb513bf2c87c722d823c8913a7
Qimat Luo <qimatluo@westpharma.com>
2024-06-14 21:14:31 +0800

    My setup

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,12 +1,17 @@
--- EXAMPLE 
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
+local servers = {
+  "cssls",
+  "docker_compose_language_service",
+  "dockerls",
+  "html",
+  "tsserver",
+  "volar",
+}
 
--- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
@@ -15,9 +20,22 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- typescript
 lspconfig.tsserver.setup {
   on_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
+  init_options = {
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = "/home/aa/.nvm/versions/node/v20.10.0/lib/node_modules/@vue/typescript-plugin",
+        languages = { "vue" },
+      },
+    },
+  },
+  filetypes = {
+    "javascript",
+    "typescript",
+    "vue",
+  },
 }
